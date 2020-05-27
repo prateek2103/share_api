f5de8d842de0e396acddb7566695441f28aea503
prateek2103 <prateek007.purohit@gmail.com>
2020-05-27 16:10:14 +0530

    adding the user model

--- models/model.js
+++ models/model.js
@@ -1,10 +1,40 @@
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 
+//registration model
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    company:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    employeeId:{
+        type:Schema.Types.ObjectId,
+        ref:"Employee"
+    }
+})
+
+//company model
 const companySchema=new Schema({
     _id:Schema.Types.ObjectId,
     name:{
         type:String,
+        unique:true,
         required:true
     },
 
@@ -14,15 +44,24 @@ const companySchema=new Schema({
     }]
 })
 
+//employee model
 const employeeSchema=new Schema({
     _id:Schema.Types.ObjectId,
     name:{
         type:String,
         required:true
     },
+    dob:{
+        type:Date,
+        required:true
+    },
     company:{
         type:Schema.Types.ObjectId,
         ref:"Company"
+    },
+    phone:{
+        type:Number,
+        required:true
     },
     projects:[{
         type:Schema.Types.ObjectId,
@@ -30,6 +69,7 @@ const employeeSchema=new Schema({
     }]
 })
 
+//project model
 const projectSchema=new Schema({
     _id:Schema.Types.ObjectId,
     name:{
@@ -42,12 +82,13 @@ const projectSchema=new Schema({
     },
     users:[{
         user:{type:Schema.Types.ObjectId,ref:"Employee"},
-        access:{type:Array,default:["read"]}
+        access:{type:Array,default:[]}
     }]
 })
 
 const Company=mongoose.model("Company",companySchema,"Company")
 const Employee=mongoose.model("Employee",employeeSchema,"Employee")
 const Project=mongoose.model("Project",projectSchema,"Project")
+const User=mongoose.model("User",userSchema,"User")
 
-module.exports={"company":Company,"employee":Employee,"project":Project}
+module.exports={"company":Company,"employee":Employee,"project":Project,"user":User}
